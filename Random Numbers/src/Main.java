1d646950092c66045a9c11e7847d6ccb4478070c
kennethdeoperilla <82525450+kennethdeoperilla@users.noreply.github.com>
2021-04-20 14:17:19 +0800

    Add files via upload

---
+++ Random Numbers/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Random random = new Random();
+		
+		int x = random.nextInt(6)+1;
+		
+		double y = random.nextDouble();
+		
+		boolean z = random.nextBoolean();
+		
+		
+		System.out.println("Here are the 3 random values");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	}
+
+}
